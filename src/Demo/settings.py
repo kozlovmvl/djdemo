f42736b8671580903f6250f40a074497a2a83079
kozlovmv <mikhail.kozlov@eventplatform.ru>
2026-03-21 10:23:14 +0300

    Create models in users app: User, City

--- src/Demo/settings.py
+++ src/Demo/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR.parent / "static"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR.parent / "media"
--- src/users/admin.py
+++ src/users/admin.py
@@ -1,2 +1,17 @@
+from django.contrib import admin
 
-# Register your models here.
+from users.models import City, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "id")
+    search = ("username", "email")
+    readonly_fields = ("created_at", "updated_at")
+    autocomplete_fields = ("city",)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "id")
+    search_fields = ("name",)
